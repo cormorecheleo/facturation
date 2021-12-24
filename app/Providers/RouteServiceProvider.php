4e399d203d60914fbb15a36a265c71801dc6e4bd
Leo Cormoreche <leocormo@gmail.com>
2021-12-24 09:17:36 +0100

    TASK-001 : Create login / register

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,7 +21,9 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/';
+
+    public const COOR = '/signup/contact-info';
 
     /**
      * Define your route model bindings, pattern filters, etc.
